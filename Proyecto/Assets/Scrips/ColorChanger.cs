d1c990d06f2b7598c2260547f83f557713484195
CesarAlexisOsunaHernandez <cesar.osuna23@uabc.edu.mx>
2022-06-06 15:41:23 -0700

    0.5
    Subiendo parte VR pt 4

---
+++ Proyecto/Assets/Scrips/ColorChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : Interactable
+{
+	private Material material;
+	
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+	
+	protected override void Interact(){
+		base.Interact();
+		float r = Random.Range(0f,1f);
+		float g = Random.Range(0f,1f);
+		float b = Random.Range(0f,1f);
+		material.color = new Color(r,g,b,1f);
+	}
+}
